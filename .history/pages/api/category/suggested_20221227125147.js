7319bc0291049c01afcbbff8b1160be6b4e3345a
haviles04 <haviles04@gmail.com>
2022-12-27 12:54:00 -0500

    created API for suggestions data

---
+++ .history/pages/api/category/suggested_20221227125147.js
@@ -0,0 +1,10 @@
+import  suggested  from '../../../data/products/suggested.json'
+
+export default function handler(req, res){
+    try{
+    res.status(200).json(suggested);}
+    catch(err){
+        console.log(err);
+        res.status(500).send({message: 'Internal Server Error'})
+    }
+}
