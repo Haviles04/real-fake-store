163b67e6ac8300f2ea443c2e218d1f72097c1cd6
haviles04 <haviles04@gmail.com>
2022-12-29 19:40:11 -0500

    deleted local history file
    deleted local history

--- .history/pages/api/category/index_20221221163712.js
+++
@@ -1,9 +0,0 @@
-import  categories  from '../../../data/category/category.json'
-
-export default function handler(req, res){
-    try{
-    res.status(200).json(categories);}
-    catch{
-        console.log('Error!')
-    }
-}
