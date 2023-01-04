fd82a343357ab64fd0f390485fe7e078c49337e6
haviles04 <haviles04@gmail.com>
2023-01-03 19:01:41 -0500

    added footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import styles from '../../styles/footer.module.css'
+import Link from "next/link";
+import styles from "../../styles/footer.module.css";
 
-const Footer = () => {
+const Footer = ({ cats }) => {
   return (
-    <div className={styles.footerContainer}>Footie</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.shopFooter}>
+        <h2>Shop</h2>
+        <ul>
+          {cats.map((cat) => (
+            <li key={cat.id}>
+              <Link href={cat.name.toLowerCase()}>{cat.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.contact}>
+        <h2>Contact Us!</h2>
+        <p>Email</p>
+        <h4>Address</h4>
+        <p>123 Electric Avenue</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
