fc6ea5f876e98441c376431d5007a1b6438031d3
haviles04 <haviles04@gmail.com>
2023-01-05 22:41:53 -0500

    prod bug

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -58,7 +58,9 @@ function ProductCard({ item }) {
         href={`/${item.category.name.toLowerCase()}/${item.id}=${item.title
           .toLowerCase()
           .replace(/\s/g, "")}`}
-        as={`/${item.category.name}/${item.id}=${item.title}`}  
+        as={`/${item.category.name.toLowerCase()}/${item.id}=${item.title
+          .toLowerCase()
+          .replace(/\s/g, "")}`}  
       >
         <Image
           className={styles.productImage}
