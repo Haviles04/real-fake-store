21a4737fff7c97cd495de28f9a836a8354d55e09
haviles04 <haviles04@gmail.com>
2023-01-05 22:40:13 -0500

    prod bug

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
 }
 
 module.exports = {
-    distDir: "_next",
-    generateBuildId: async () => {
-      if (process.env.BUILD_ID) {
-        return process.env.BUILD_ID;
-      } else {
-        return `${new Date().getTime()}`;
-      }
-    },
   
   images:{
     remotePatterns: [
