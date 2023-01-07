5482256508c28376754c6fe58f19770368c1d4c9
haviles04 <haviles04@gmail.com>
2023-01-07 08:29:40 -0500

    changed data handling for dynamic page routing

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps, cats}) {
 }
 
 App.getInitialProps = async () => {
-  const {cats} = await import(`../data/category/categoryData.json`)
+  const {cats} = await import(`../data/data.json`)
   return {
     cats,
   };
